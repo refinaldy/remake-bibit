950fe15daa7469b512e2b28afa1d2738216efc24
Refinaldy Madras <refimadras.code@gmail.com>
2020-12-20 18:24:42 +0700

    add variable search view

--- app/src/main/java/com/refinaldy/newbibit/MainActivity.kt
+++ app/src/main/java/com/refinaldy/newbibit/MainActivity.kt
@@ -2,12 +2,14 @@ package com.refinaldy.newbibit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val search: SearchView = findViewById(R.id.searchView)
 
     }
 
