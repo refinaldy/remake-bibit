c53891d7cc8bf93b7ec0a59edcd17c733f4aade6
Refinaldy Madras <refimadras.code@gmail.com>
2021-01-13 21:35:54 +0700

    refactor all item id for better readable.

--- app/src/main/java/com/refinaldy/newbibit/menu_fragment/HomeFragment.kt
+++ app/src/main/java/com/refinaldy/newbibit/menu_fragment/HomeFragment.kt
@@ -15,4 +15,11 @@ class HomeFragment : Fragment() {
 
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 }
