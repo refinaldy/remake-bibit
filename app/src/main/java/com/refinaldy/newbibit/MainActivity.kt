623b7450b87b155b2f2f0e097681f9513fee0d33
Refinaldy Madras <refimadras.code@gmail.com>
2021-01-13 12:49:42 +0700

    Change main activity for implement fragment and add bottom navbar to main activity.

--- app/src/main/java/com/refinaldy/newbibit/MainActivity.kt
+++ app/src/main/java/com/refinaldy/newbibit/MainActivity.kt
@@ -2,16 +2,42 @@ package com.refinaldy.newbibit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SearchView
+import androidx.fragment.app.Fragment
+import com.refinaldy.newbibit.menu_fragment.*
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val search: SearchView = findViewById(R.id.searchView)
-    
+        val homeFragment = HomeFragment()
+        val portofolioFragment = PortofolioFragment()
+        val transactionFragment = TransactionFragment()
+        val supportFragment = SupportFragment()
+        val profileFragment = ProfileFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_portofolio -> makeCurrentFragment(portofolioFragment)
+                R.id.ic_transaction -> makeCurrentFragment(transactionFragment)
+                R.id.ic_support -> makeCurrentFragment(supportFragment)
+                R.id.ic_profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+
+        }
     }
 
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 
 }
