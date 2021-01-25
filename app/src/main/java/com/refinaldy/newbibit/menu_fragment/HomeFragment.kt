02b8ddccdc78b1c3a9b326b5c28f687e26e49a55
refinaldy7 <refimadras.code@gmail.com>
2021-01-25 23:40:07 +0700

    fix minor error

--- app/src/main/java/com/refinaldy/newbibit/menu_fragment/HomeFragment.kt
+++ app/src/main/java/com/refinaldy/newbibit/menu_fragment/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.refinaldy.newbibit.menu_fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import com.refinaldy.newbibit.DetailReksadanaActivity
 import com.refinaldy.newbibit.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -21,5 +24,14 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ic_rdpu.setOnClickListener{
+            val toDetailActivity = Intent(activity, DetailReksadanaActivity::class.java)
+            startActivity(toDetailActivity);
+        }
     }
 }
